afa42437f501d0a7a8759b3fc756cee9545fe185
Brandon Murray <bmurmistro@gmail.com>
2020-10-06 13:05:52 -0600

    b11

--- test/example-classic.js
+++ test/example-classic.js
@@ -41,7 +41,7 @@ describe('wdio5', function () {
         configuration.setApiKey(process.env.APPLITOOLS_API_KEY)
         
         const batch = new BatchInfo("B");
-        batch.setId("b10")
+        batch.setId("b11")
         // Set new batch
         configuration.setBatch(batch)
         // Set the configuration to eyes
